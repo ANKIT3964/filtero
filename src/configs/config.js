be0a97b24e83a7e0d0d2412026061cc99922f4a2
ANKIT-COA <112258010+ANKIT-COA@users.noreply.github.com>
2024-06-24 23:05:20 +0530

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -24,19 +24,19 @@ const createStringSession = makeValidator(
 
 export default cleanEnv(process.env, {
   // User Credentials
-  APP_ID: num(12512870),
-  API_HASH: str(01e4639ae903f5d4a7b0876e5a3ea0a1),
-  STRING_SESSION: createStringSession(1BVtsOMABu5QQKYwpd7A5xBepxnkADbuAUKz12FXF9EauVIcUqlKJdJ16KCMtYVTqEZgFcPTLqIbDiWaXOlDcxRfym_uq1Mp7Jw8b3aAmtCrKaGcw8ZVuNkROoV44PgBFvQTj8f1x6CO6MFMPuSzYFdOrNtmVooke9mjlncMk-nIxWjnDgSz1B6LSq5HES9FqrWqI4c8Ms8mD05LyikkH-3U2kELKCSCpywmLwsCmIKQQIDK-QUjbuoP6LHHCeU0XbJlFRmVzjuNd8Hec1btps28r6gcV14vllALSN-TeBHWi6_oB5SRRgu2PZYaP2DC50zm4L1VgYhon-U_f33SnO6_mCJ6lI6o=),
+  APP_ID: num(),
+  API_HASH: str(),
+  STRING_SESSION: createStringSession(),
 
   // Bot Credentials
-  BOT_TOKEN: str(5638053781:AAFJYQhDLXnZCvVM0jG_MSPbuVuHZ9us01A),
-  AUTHORIZED_USERS: splitArrayAndFilterIntegers(1197918807),
+  BOT_TOKEN: str(),
+  AUTHORIZED_USERS: splitArrayAndFilterIntegers(),
 
   // MySQL Database Credentials
-  DATABASE_NAME: str(u597183945_sigmaankit),
-  DATABASE_USER: str(u597183945_sigmaankit),
-  DATABASE_USER_PASSWORD: str(#Sigmaankit2025#),
-  DATABASE_HOST: str(srv1327.hstgr.io),
+  DATABASE_NAME: str(),
+  DATABASE_USER: str(),
+  DATABASE_USER_PASSWORD: str(),
+  DATABASE_HOST: str(),
   DATABASE_PORT: num({ default: 3306 }),
   AUTHORIZED_CHAT_IDS: splitArray(),
 
